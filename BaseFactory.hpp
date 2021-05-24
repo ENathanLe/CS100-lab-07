3dbb7e69c475d03736ed9e8f95edc2ee48c35540
toothnnail <michael.s.dong@gmail.com>
2021-05-24 04:54:10 -0700

    added initial flags for basefact

--- BaseFactory.hpp
+++ BaseFactory.hpp
@@ -10,7 +10,21 @@
 #include "sub.hpp"
 
 class BaseFactory : public Base {
+	Base* parse(char** input, int length) 	{
+		//===========================================initial=screening==============================================
+		/* input ex: ./calculator, X, Y, Z 
+		 * length must be EVEN (ODD equation length + 1 (./calculator argument))
+		 * equation can't be empty or contain only a single operator/operand
+		 * should not begin or end with an operator
+		 */		
+		if(length % 2 == 1 || length <= 2 || !isdigit(*input[i]) || !isdigit(*input[length-1])) { return nullptr; } 
+		//==========================================================================================================
+		
+		Base* val1 = new Op(atof(input[1])); 
 
+
+
+	}
 };
 
 #endif //__BASE_FACTORY_HPP__
